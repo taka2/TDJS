5289c3dbc051eb75974baf91f9d383c6b8243bb9
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2012-03-21 13:20:07 +0900

    wsh向けtest.jsを作成。

---
+++ testwsh.js
@@ -0,0 +1,50 @@
+function output(text, color) {
+  //var p = document.createElement("p");
+  //p.innerHTML = text;
+  //p.style.color = color;
+  //document.body.appendChild(p);
+  WScript.Echo(color + ": " + text);
+}
+
+function assert(message, expr) {
+  if(!expr) {
+    throw new Error(message);
+  }
+  
+  assert.count++;
+  return true;
+}
+
+function testCase(name, tests) {
+  assert.count = 0;
+  var successful = 0;
+  var testCount = 0;
+  var hasSetup = typeof tests.setUp == "function";
+  var hasTeardown = typeof tests.tearDown == "function";
+  for(var test in tests) {
+    if(!/^test/.test(test)) {
+      continue;
+    }
+    
+    testCount++;
+    
+    try {
+      if(hasSetup) {
+        tests.setUp();
+      }
+      tests[test]();
+      output(test, "#0c0");
+      if(hasTeardown) {
+        tests.tearDown();
+      }
+      successful++;
+    } catch(e) {
+      output(test + " failed: " + e.message, "#c00");
+    }
+  }
+  
+  var color = successful == testCount ? "#0c0" : "#c00";
+  output("<strong>" + testCount + " tests, " + 
+         (testCount - successful) + " failures</strong>",
+         color);
+}
